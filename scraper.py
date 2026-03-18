f4419907d32c98d80bb5a33bb77ebfa41f9d4e79
Enes <enessceng@gmail.com>
2026-03-19 00:35:19 +0300

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 import pandas as pd
 import time
 from datetime import datetime
