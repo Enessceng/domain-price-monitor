57bee310634580e98f00c7743549d810bd510e99
Enes <enessceng@gmail.com>
2026-03-19 00:50:12 +0300

    new one

--- scraper.py
+++ scraper.py
@@ -25,11 +25,23 @@ logging.info("Script started")
 
 # DRIVER
 
-driver = webdriver.Chrome()
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=chrome_options
+)
+
 wait = WebDriverWait(driver, 20)
 
 wanted = [".co", ".com", ".org", ".io", ".net"]
-
 
 # DYNADOT
 
